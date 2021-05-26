27f32b5b56d7c53e2e5225e0c6a10036725d204c
Carlos Guerrero <ca.guerrero@uniandes.edu.co>
2021-05-26 02:38:47 -0500

    Finished PWA

--- src/components/Series.js
+++ src/components/Series.js
@@ -1,21 +1,60 @@
 import { useEffect, useState } from "react";
 import { Table } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 const Series = () => {
     const [series, setSeries] = useState([]);
+
+    const [state, setState] = useState([]);
+
+    const clickedSerie = (id) => {
+        let newArr = [...state];
+        if (newArr[id]) {
+            newArr[id] = false;
+            setState(newArr);
+        } else {
+            let i = 0;
+            while (i < newArr.length) {
+                newArr[i] = false;
+                i++;
+            }
+            newArr[id] = true;
+            setState(newArr);
+        }
+    }
+
     useEffect(() => {
-        fetch("https://gist.githubusercontent.com/josejbocanegra/c55d86de9e0dae79e3308d95e78f997f/raw/a467415350e87c13faf9c8e843ea2fd20df056f3/series-es.json")
+        if (!navigator.onLine) {
+            let i = 0;
+            const arrSeries = [];
+            while (localStorage.getItem(`series ${i}`) !== null) {
+                arrSeries.push(JSON.parse(localStorage.getItem(`series ${i}`)));
+                i++;
+            }
+            if (arrSeries.length < 0) {
+                arrSeries.push({ id: 0, name: "Error while loading", channel: "Error while loading", seasons: 0, episodes: 0, release: "0/0/0", description: "Error while loading", webpage: "Error while loading", poster: "Error while loading image." });
+            }
+            arrSeries.forEach((item)=>{
+                item.poster = "Error while loading image.";
+            })
+            setSeries(arrSeries);
+        } else{
+            fetch("https://gist.githubusercontent.com/josejbocanegra/c55d86de9e0dae79e3308d95e78f997f/raw/a467415350e87c13faf9c8e843ea2fd20df056f3/series-es.json")
             .then(result => result.json())
             .then((result) => {
                 Object.keys(result).forEach((key) => {
-                    setSeries(series => [...series, result[key]])
+                    localStorage.setItem(`series ${key}`, JSON.stringify(result[key]));
+                    setSeries(series => [...series, result[key]]);
+                    setState(state => [...state, false]);
                 })
             })
+        }
     }, []);
+
 
     return (
         <>
-            <Table bordered striped hover>
+            <Table bordered striped hover style={{ width: "40rem" }}>
                 <thead>
                     <tr>
                         <th>#</th>
@@ -28,7 +67,7 @@ const Series = () => {
                 </thead>
                 <tbody>
                     {series.map((item) => (
-                        <tr>
+                        <tr onClick={() => clickedSerie(item.id)}>
                             <td>{item.id}</td>
                             <td>{item.name}</td>
                             <td>{item.channel}</td>
@@ -39,20 +78,23 @@ const Series = () => {
                     ))}
                 </tbody>
             </Table>
+            {
+                series.map((item) => (
+                    state[item.id] && (<Card style={{ width: "30rem" }}>
+                        {item.poster!=="Error while loading image." && <Card.Img variant="top" src={item.poster}/>}
+                        {item.poster==="Error while loading image." && <Card.Text> {item.poster} </Card.Text>}
+                        <Card.Body>
+                            <Card.Title>{item.name}</Card.Title>
+                            <Card.Text>
+                                {item.description}
+                            </Card.Text>
+                            <Card.Link href={item.webpage}>{item.webpage}</Card.Link>
+                        </Card.Body>
+                    </Card>)
+                ))
+            }
         </>
     )
-
-    /*
-    "id": 2,
-    "name": "Orange Is the New Black",
-    "channel": "Netflix",
-    "seasons": 6,
-    "episodes": 91,
-    "release": "11/07/2013",
-    "description": "La serie gira en torno a Piper Chapman, una mujer de unos treinta años que vive en la ciudad de Nueva York y que es sentenciada a 15 meses en la penitenciaría de Litchfield.",
-    "webpage": "https://www.netflix.com/co/title/70242311",
-    "poster": "https://i.imgur.com/EvKe48G.jpg"
-*/
 }
 
 export default Series;
