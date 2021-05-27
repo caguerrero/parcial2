a30318a6008eef1827cda39be3f9511423a6737b
Carlos Guerrero <ca.guerrero@uniandes.edu.co>
2021-05-26 19:55:37 -0500

    Visual

--- src/components/Series.js
+++ src/components/Series.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
+import { useEffect, useState, useRef } from 'react';
+import { Table} from 'react-bootstrap';
 import Serie from './Serie';
+import * as d3 from 'd3';
 import { FormattedDate, FormattedNumber, FormattedMessage } from 'react-intl';
 
 function convert(date) {
@@ -10,6 +11,9 @@ function convert(date) {
 }
 
 const Series = (props) => {
+
+    const canvasRef = useRef();
+
     const [series, setSeries] = useState([]);
 
     const [state, setState] = useState([]);
@@ -56,11 +60,103 @@ const Series = (props) => {
                     })
                 })
         }
-    }, []);
+    }, [props.errMsg, props.lang]);
 
+    useEffect(() => {
+        const chart = () => {
+            const width = 800;
+            const height = 350;
+            const margin = { top: 40, left: 30, bottom: 40, right: 30 };
+            const iwidth = width - margin.left - margin.right;
+            const iheight = height - margin.top - margin.bottom;
+
+            const svg = d3
+                .select(canvasRef.current)
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+            let g = svg
+                .append("g")
+                .attr("transform", `translate(${margin.left},${margin.top})`);
+
+            let yMin = Math.min.apply(
+                Math,
+                series.map(function (o) {
+                    return o.seasons;
+                })
+            );
+            let yMax = Math.max.apply(
+                Math,
+                series.map(function (o) {
+                    return o.seasons;
+                })
+            );
+            let xMin = Math.min.apply(
+                Math,
+                series.map(function (o) {
+                    return o.episodes;
+                })
+            );
+            let xMax = Math.max.apply(
+                Math,
+                series.map(function (o) {
+                    return o.episodes;
+                })
+            );
+
+            const y = d3
+                .scaleLinear()
+                .domain([yMin - 2, yMax])
+                .range([iheight, 0]);
+
+            g.append("g").call(d3.axisLeft(y));
+
+            const x = d3
+                .scaleLinear()
+                .domain([xMin - 5, xMax + 100])
+                .range([0, iwidth]);
+
+            g.append("g")
+                .attr("transform", "translate(0," + iheight + ")")
+                .call(d3.axisBottom(x));
+
+            let circles = g.append("g").selectAll("dot").data(series).enter();
+
+            circles
+                .append("circle")
+                .attr("cx", function (d) {
+                    return x(parseInt(d.episodes));
+                })
+                .attr("cy", function (d) {
+                    return y(parseInt(d.seasons));
+                })
+                .attr("r", 6)
+                .style("fill", "#ffd105")
+                .style("opacity", "0.7")
+                .attr("stroke", "black");
+
+            circles
+                .append("text")
+                .attr("class", "label")
+                .style("font-size", "12px")
+                .attr("y", function (d) {
+                    return y(d.seasons);
+                })
+                .attr("x", function (d) {
+                    return x(d.episodes + 10);
+                })
+                .text(function (d) {
+                    return d.name;
+                });
+        };
+        if (series.length === 6) {
+            chart();
+        }
+    }, [series])
 
     return (
         <>
+            <h1><FormattedMessage id="Title" /></h1>
             <Table bordered striped hover style={{ width: "40rem" }}>
                 <thead>
                     <tr>
@@ -102,9 +198,10 @@ const Series = (props) => {
             </Table>
             {
                 series.map((item) => (
-                    state[item.id] && <Serie key={item.id} it={item} errMsg= {props.errMsg} />
+                    state[item.id] && <Serie key={item.id} it={item} errMsg={props.errMsg} />
                 ))
             }
+            <div ref={canvasRef} className="chart"></div>
         </>
     )
 }
